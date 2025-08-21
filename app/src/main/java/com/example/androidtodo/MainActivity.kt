2d1a5dc6be267e3d070421b67744ecc3bc41d8a7
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-08-21 18:59:48 +0300

    feat: create basic UI

--- app/src/main/java/com/example/androidtodo/MainActivity.kt
+++ app/src/main/java/com/example/androidtodo/MainActivity.kt
@@ -4,44 +4,123 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.androidtodo.ui.theme.AndroidTodoTheme
+import com.example.androidtodo.ui.theme.Typography
+
+data class Todo(val text: String, val done: Boolean)
+
+var todos = arrayOf(
+    Todo("First todo", false),
+    Todo("Second todo", true),
+)
+
+@Composable
+fun RemainderText(remaining: Int, total: Int) {
+    Text("$remaining of $total remaining", modifier = Modifier.padding(bottom = 32.dp))
+}
+
+@Composable
+fun TodoItem(todo: Todo) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(checked = todo.done, onCheckedChange = {})
+            Text(
+                todo.text,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth(0.65f)
+            )
+        }
+        Button(
+            {}, colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Delete")
+        }
+    }
+}
+
+@Composable
+fun TodoList(todos: Array<Todo>) {
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        todos.map { TodoItem(it) }
+    }
+}
+
+@Composable
+fun App() {
+    Text(text = "Tasks", style = Typography.titleLarge)
+    RemainderText(0, 0)
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.padding(bottom = 32.dp)
+    ) {
+        OutlinedTextField(
+            value = "",
+            {},
+            placeholder = { Text("Add a new task...") },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary
+            )
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Text("+")
+        }
+    }
+    TodoList(todos)
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidTodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            AndroidTodoTheme(dynamicColor = false) {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier
+                            .padding(top = 64.dp, start = 24.dp, end = 24.dp)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        App()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTodoTheme {
-        Greeting("Android")
     }
 }
